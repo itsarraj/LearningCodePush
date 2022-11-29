5489cd2dcfbe763d5b6d7967dd0e710a510fe683
Animesh Raj <Animesh Raj>
2022-11-29 15:58:08 +0530

    Completed HTML

---
+++ learning.js
@@ -0,0 +1,20 @@
+// Normal Function
+
+function add(a, b) {
+  return a + b;
+}
+
+// Arrow Function
+const add = (a, b) => a + b;
+
+// or
+
+const add = (a, b) => {
+  return a + b;
+};
+
+// Symbols
+// for === they wont be equal ,
+// they are used to make objects that must not collide;
+
+//Adavnced Array
